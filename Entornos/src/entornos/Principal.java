a8218cf48b1ed0bfab18d4ff69a0d65308a21db8
Dani <Dani@DESKTOP-G6KMRJ1>
2022-05-11 21:08:45 +0200

    Añadido registro

--- Entornos/src/entornos/Principal.java
+++ Entornos/src/entornos/Principal.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Principal {
 
@@ -12,6 +13,8 @@ public class Principal {
 	static ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 	
 	static ArrayList<Administrador> admins = new ArrayList<Administrador>();
+	
+	static Scanner sc = new Scanner(System.in);
 	
 	public static void crearCarpetas() throws FileNotFoundException {
 		File carpeta = new File("C:\\dCuadrado");
@@ -24,16 +27,23 @@ public class Principal {
 		carpetaUsuarios.mkdir();
 	}
 	
-	public static void registro(int num) {
+	public static boolean registro(int num) {
+		boolean usuarioEncontrado = false;
 		for (Usuario i:  usuarios) {
-			if (i.getTelefono() == num)
-				administrador = false; 
+			if (i.getTelefono() == num) {
+				administrador = false;
+				usuarioEncontrado = true;
+			}
 		}
 		for (Administrador i:  admins) {
-			if (i.getTelefono() == num)
+			if (i.getTelefono() == num) {
 				administrador = true;
+				usuarioEncontrado = true;
+			}
 		}
+		return usuarioEncontrado;
 	}
+	
 	
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
@@ -51,8 +61,25 @@ public class Principal {
 		
 		admins.add(new Administrador(123123123));
 		
-		registro(123456789);
-		
-		System.out.println(administrador);
+		for(boolean valido = false ; !valido;) {
+			System.out.print("\nIntroduce el numero de usuario: ");
+			int num = sc.nextInt();
+			valido = registro(num);
+			if (!valido) {
+				if (String.valueOf(num).length() <9)
+					System.out.println("\nEl numero tiene menos de 9 digitos");
+				else if (String.valueOf(num).length() >9) {
+					System.out.println("\nEl numero tiene más de 9 digitos");
+				}
+				else if (!valido)
+					System.out.println("\nUsuario no encontrado");
+			} else {
+				if (administrador)
+					System.out.println("\nTe has registrado como administrador");
+				else {
+					System.out.println("\nTe has registrado como usuario");
+				}
+			}
+		}
 	}
 }
