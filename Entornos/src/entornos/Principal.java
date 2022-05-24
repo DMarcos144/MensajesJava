0833ebd447fd2b75c2fd7468e0057c71a0fea941
Wenut <Wenut@DESKTOP-OC9RNRP>
2022-05-24 17:03:26 +0200

    Creación de menú tanto de administrador como de usuario.

--- Entornos/src/entornos/Principal.java
+++ Entornos/src/entornos/Principal.java
@@ -11,6 +11,8 @@ public class Principal {
 
 	static boolean administrador;
 	
+	static String usuarioActivo;
+
 	static ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 	
 	static ArrayList<Administrador> admins = new ArrayList<Administrador>();
@@ -57,8 +59,39 @@ public class Principal {
 		}
 		return usuarioEncontrado;
 	}
-	
-	
+
+	public static void iniciarSesion() {
+		String num;
+		for(boolean valido = false ; !valido;) {
+
+			try {
+				System.out.print("\nIntroduce el numero de usuario: ");
+				Scanner sc = new Scanner(System.in);
+				num = Integer.toString(sc.nextInt());
+				valido = registro(num);
+				if (!valido) {
+					if (num.length() <9)
+						System.out.println("\nEl numero tiene menos de 9 digitos");
+					else if (num.length() >9) {
+						System.out.println("\nEl numero tiene más de 9 digitos");
+	 				}
+					else if (!valido)
+						System.out.println("\nUsuario no encontrado");
+				} else {
+					if (administrador) {
+						System.out.println("\nTe has registrado como administrador\n");
+						usuarioActivo = num;
+					}else {
+						System.out.println("\nTe has registrado como usuario");
+						usuarioActivo = num;
+					}
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("\nSe han introducido caracteres no válidos");
+			}
+		}
+	}
+
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		
@@ -76,41 +109,35 @@ public class Principal {
 		admins.add(new Administrador("123123123"));
 		boolean salir = false;
 		while (!salir) {
-			System.out.print("Selecciona una opción (1-8): ");
+			if (usuarioActivo == null)
+				iniciarSesion();
+			
+			if(!administrador) {	
+				System.out.println("	Menú de Usuario");
+				System.out.println("\n1: Enviar mensaje");
+				System.out.println("\n2: Ver mensajes enviados");
+				System.out.println("\n3: Ver mensajes recibidos");
+				System.out.println("\n4: Ver mensajes de:");
+				System.out.println("\n5: Ver lista de contactos");
+				System.out.println("\n6: Cerrar sesión");
+			} else {
+				System.out.println("	Menú de Administrador");
+				System.out.println("\n1: Enviar mensaje");
+				System.out.println("\n2: Ver mensajes enviados");
+				System.out.println("\n3: Ver mensajes recibidos");
+				System.out.println("\n4: Ver mensajes de:");
+				System.out.println("\n5: Ver mensajes enviados de un contacto");
+				System.out.println("\n5: Ver mensajes recibidos de un contacto");
+				System.out.println("\n6: Ver lista de contactos");
+				System.out.println("\n7: Cerrar sesión");
+			}
 			int eleccion = pedirNumero();
-			String num;
 			switch (eleccion) {
-				case 1:
-						for(boolean valido = false ; !valido;) {
-							
-							try {
-								System.out.print("\nIntroduce el numero de usuario: ");
-								Scanner sc = new Scanner(System.in);
-								num = Integer.toString(sc.nextInt());
-								valido = registro(num);
-								if (!valido) {
-									if (num.length() <9)
-										System.out.println("\nEl numero tiene menos de 9 digitos");
-									else if (num.length() >9) {
-										System.out.println("\nEl numero tiene más de 9 digitos");
-									}
-									else if (!valido)
-										System.out.println("\nUsuario no encontrado");
-								} else {
-									if (administrador)
-										System.out.println("\nTe has registrado como administrador");
-									else {
-										System.out.println("\nTe has registrado como usuario");
-									}
-								}
-							} catch (InputMismatchException e) {
-								System.out.println("\nSe han introducido caracteres no válidos");
-								num = "111111111";
-						}
-					}
-				break;
-				default:
-					System.out.println("a");
+			case 8:
+				usuarioActivo = null;
+			break;
+			default:
+				System.out.println("a");
 			}
 		}
 	}
